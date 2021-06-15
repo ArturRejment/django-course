f79cbfeb1acaffda4edbdff2904087ed590594fb
Artur Rejment <arturrejment1@gmail.com>
2021-06-15 14:38:47 +0200

    Login and logout user

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutPage, name="logout"),
 
 
     path('', views.dashboard, name='dashboard'),
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .forms import *
 from .filters import *
@@ -116,12 +118,36 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'User ' + user + ' created!')
+            return redirect('login')
 
 
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+
+        else:
+            messages.info(request, 'Username or password is incorrect')
+
+
     context = {}
     return render(request, 'accounts/login.html', context)
+
+
+def logoutPage(request):
+
+    logout(request)
+
+    return redirect('login')
